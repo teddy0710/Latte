3b1103cde707c95f5c1026d49f3bb4c003284a28
ZFL <teddy19930521@gmail.com>
2017-08-27 03:25:22 +0800

    5.1+5.2 网络请求框架接口创建

---
+++ latte-core/src/main/java/com/example/latte/net/HttpMethod.java
@@ -0,0 +1,15 @@
+package com.example.latte.net;
+
+/**
+ * Created by 张枫霖 on 2017/8/27
+ */
+
+public enum HttpMethod {
+    GET,
+    POST,
+    PUST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UOLOAD
+}
---
+++ latte-core/src/main/java/com/example/latte/net/RestClient.java
@@ -0,0 +1,8 @@
+package com.example.latte.net;
+
+/**
+ * Created by 张枫霖 on 2017/8/27
+ */
+
+public class RestClient {
+}
