d9a00e01d1b78e49b55c201d4750c574817ddebe
ZFL <teddy19930521@gmail.com>
2017-08-28 12:17:31 +0800

    5.5 loading框架集成与完善 AVLoadingIndicator - 1

---
+++ latte-core/src/main/java/com/example/latte/ui/LatteLoader.java
@@ -0,0 +1,16 @@
+package com.example.latte.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+
+
+/**
+ * Created by 张枫霖 on 2017/8/28
+ */
+
+public class LatteLoader {
+    public static void showLoading(Context context, String type) {
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+
+    }
+}
